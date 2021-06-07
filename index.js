e1cdafaa9d9df510c2cf8e626b23101b2a20a739
Ridhima jain <sakshisona21@gmail.com>
2021-06-07 11:41:15 +0530

    my first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var marks=100;
+// marks=50 //
+console.log(marks);
+
+var num1 ,num2,result;
+num1=20;
+num2 =40;
+result=num2 % num1;
+console.log(result);
+
+result= num2 / num1;
+console.log(result);
+
+if(num1 > num2)
+console.log(  num1+ "  greater than " +num2);
+
+if (num2 > num1)
+console.log( num2+ "  greater than " +num1 );
+
+var aru = "ridhima"+ "love"+ "mr"+ "."+ "bakliwal";
+console.log(aru);
